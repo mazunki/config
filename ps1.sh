e002105799914208b82de57f1dade8e6d134e239
mazunki <rolferen@gmail.com>
2019-09-29 18:02:33 +0200

    moved colours

--- ps1.sh
+++ ps1.sh
@@ -22,7 +22,7 @@ GET_BRANCH() {
 
 		else
 			echo -en "${MAGENTA}[${LYELLOW}$(GIT_BRANCH)${MAGENTA}]${RESTORE}"
-			
+
 		fi
 	fi
 }
@@ -45,24 +45,6 @@ match_lhs=""
 	&& type -P dircolors >/dev/null \
 	&& match_lhs=$(dircolors --print-database)
 [[ $'\n'${match_lhs} == *$'\n'"TERM "${safe_term}* ]] && use_color=true
-
-RESTORE=$(echo -en '\001\033[0m\002')
-RED=$(echo -en '\001\033[00;31m\002')
-GREEN=$(echo -en '\001\033[00;32m\002')
-YELLOW=$(echo -en '\001\033[00;33m\002')
-BLUE=$(echo -en '\001\033[00;34m\002')
-MAGENTA=$(echo -en '\001\033[00;35m\002')
-PURPLE=$(echo -en '\001\033[00;35m\002')
-CYAN=$(echo -en '\001\033[00;36m\002')
-LIGHTGRAY=$(echo -en '\001\033[00;37m\002')
-LRED=$(echo -en '\001\033[01;31m\002')
-LGREEN=$(echo -en '\001\033[01;32m\002')
-LYELLOW=$(echo -en '\001\033[01;33m\002')
-LBLUE=$(echo -en '\001\033[01;34m\002')
-LMAGENTA=$(echo -en '\001\033[01;35m\002')
-LPURPLE=$(echo -en '\001\033[01;35m\002')
-LCYAN=$(echo -en '\001\033[01;36m\002')
-WHITE=$(echo -en '\001\033[01;37m\002')
 
 
 if ${use_color} ; then
