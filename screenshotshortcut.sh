a161ef969b1fefa74b59463ba8b6c7cded818746
mazunki <rolferen@gmail.com>
2020-01-30 01:31:48 +0100

    created screenshot shortcut

---
+++ screenshotshortcut.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# we want to store our original mouse position
+rawoldmouse=$(xdotool getmouselocation)
+mouseoldX=$(echo ${rawoldmouse} | sed "s/ screen.*//g" | cut -d" " -f1 | cut -c 3-)
+mouseoldY=$(echo ${rawoldmouse} | sed "s/ screen.*//g" | cut -d" " -f2 | cut -c 3-)
+
+# shortcut to screenshot applet
+xdotool mousemove 1894 1099 # x, y of your applet icon
+xdotool click 1
+xdotool mousemove 1844 1232 # x, y of your screenshot to run
+xdotool click 1
+
+# back to where mouse was
+xdotool mousemove ${mouseoldX} ${mouseoldY}
