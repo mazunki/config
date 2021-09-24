a011e1b5f21ad60d7d047ef2ba4498bfddb61dc3
mazunki <rolferen@gmail.com>
2021-09-24 15:15:10 +0200

    fixed resolution on laptop

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,10 +1,14 @@
 #!/bin/zsh
 
+confdir=${XDG_CONFIG_HOME:-$HOME/.config}
+
 source $ZDOTDIR/history.zsh
 source $ZDOTDIR/autocomplete.zsh
 source $ZDOTDIR/opts.zsh
 source $ZDOTDIR/prompt.zsh
 source $ZDOTDIR/bindings.zsh
-source $HOME/.config/aliases
-source $HOME/.config/environment
 
+source $confdir/aliases
+source $confdir/environment
+
+eval "$(dircolors -b $confdir/dircolors)"
--- zsh/autocomplete.zsh
+++ zsh/autocomplete.zsh
@@ -1,9 +1,40 @@
 #!/bin/zsh
 
-autoload -U compinit promptinit
-compinit -d $XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION
-zstyle ':completion:*' cache-path $XDG_CACHE_HOME/zsh/zcompcatche
-setopt COMPLETE_IN_WORD
-setopt MENU_COMPLETE
+# loads completiosn
+fpath=(~/.local/share/zsh/plugins/zsh-completions/src $fpath)
+
+zmodload zsh/complist
+
+autoload -U compinit promptinit; compinit -d $XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION
+
+_comp_options+=(globdots)
+
+# speeding up completion with cache
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path $XDG_CACHE_HOME/zsh/zcompcache
+
+zstyle ':completion:*' menu select  # autocomplete with navigation
+zstyle ':completion:*' completer _extensions _complete _approximate
+zstyle ':completion:*' fuzzy
+
+
+# :completion:<function>:<completer>:<command>:<argument>:<tag>
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
+zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
+
+zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
+
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+
+setopt COMPLETE_IN_WORD # allows tabbing from the start of the word too
+# setopt MENU_COMPLETE  # tab once to get first element, instead of twice
 setopt LIST_ROWS_FIRST
+setopt AUTO_LIST  # in theory resolves ambiuguous listing, not really sure
+setopt GLOB_COMPLETE  # allows for globbing in autocomplete
+
 
