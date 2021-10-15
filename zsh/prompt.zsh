fbc0d9b316363a58b274c45dde112de5dca5c957
Rolf Vidar Mazunki Hoksaas <rolferen@gmail.com>
2021-10-15 11:01:41 +0200

    created beautiful git colouring based on stage phase

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,7 +2,7 @@
 # printcolors is nice
 
 autoload -Uz add-zsh-hook vcs_info
-setopt prompt_subst
+setopt PROMPT_SUBST
 
 function check_if_git_behind() {
 	git status -sb 2> /dev/null | grep -s 'behind' 2>&1 > /dev/null
@@ -20,15 +20,18 @@ precmd_functions+=(check_if_git_behind)
 add-zsh-hook precmd vcs_info
 # %b is branch, %u is unstagedstr, %c is staged
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' unstagedstr '%F{208}●%f'
-zstyle ':vcs_info:*' stagedstr '%F{034}●%f'
-zstyle ':vcs_info:*' behindstr '%F{124}%f'
+#zstyle ':vcs_info:*' unstagedstr '%F{208}●%f'
+#zstyle ':vcs_info:*' stagedstr '%F{034}●%f'
+zstyle ':vcs_info:*' unstagedstr '%F{196}'
+zstyle ':vcs_info:*' stagedstr '%F{226}'
+zstyle ':vcs_info:*' behindstr '%F{124}!!%f'
 
-zstyle ':vcs_info:git:*' formats       '(%b%u%c) '
+zstyle ':vcs_info:git:*' formats       '(%F{046}%c%u%b%f%f) '
 zstyle ':vcs_info:git:*' actionformats '(%b|%a%u%c) '
 
 export NEWLINE=$'\n'
 rightarrow=$(echo -en '\u27f6')
 export PROMPT2="%F{239}${rightarrow}%f"
-export PROMPT='${vcs_info_msg_0_}%B%F{69}%~%f%b${NEWLINE}'
+export PROMPT='[$?] ${vcs_info_msg_0_}%B%F{69}%~%f%b${NEWLINE}'
+#export PROMPT='${vcs_info_msg_0_}%F{69}%~%f${NEWLINE}'
 export RPROMPT='%F{95}[%?]%f %F{126}%m%f'
