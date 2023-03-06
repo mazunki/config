12267c3bb99719469aaff9a81be0f7ca39348904
Mazunki Hoksaas <rolferen@gmail.com>
2023-03-06 11:21:46 +0100

    safe

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -2,7 +2,7 @@
 
 export HISTFILE="${XDG_CACHE_HOME:?history.zsh: no cache home found}/zsh/history"
 export HISTSIZE=2147483647  # /usr/include/limits.h assuming 32bit. should be nuf lolol
-export SAVEHIST=$HISTSIZE
+export SAVEHIST="${HISTSIZE}"
 setopt HIST_IGNORE_ALL_DUPS
 setopt EXTENDED_HISTORY
 setopt INC_APPEND_HISTORY
